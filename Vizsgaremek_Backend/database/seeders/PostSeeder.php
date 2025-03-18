9fa13cc2be8561e59a4c85fcb24b857bb41e2cf2
Rendszergazda <rendszergazda@blathy.info>
2025-03-18 09:38:07 +0100

    second commit started work on seeders

---
+++ Vizsgaremek_Backend/database/seeders/PostSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        for($i=0; $i<10; $i++){
+            DB::table('posts')->insert({
+                'title' => Str::random(100),
+                'content' => Str::random(100),
+            })
+
+        }
+    }
+}
