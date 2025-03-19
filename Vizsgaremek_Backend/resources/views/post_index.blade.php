2f299beb4c1527b7ea26199b82f9adec843aa9a5
SzamoskoziMate <szamoskozi.mate@blathy.info>
2025-03-19 09:57:20 +0100

    made 4 views for "PostController"

---
+++ Vizsgaremek_Backend/resources/views/post_index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Post</title>
+</head>
+<body>
+    <form action="/post" method='GET'>
+        <input type="text" name="search" class="form-control" placholder="Search">
+    </form>
+
+    @php
+        $num=1
+    @endphp
+    <table>
+        @foreach ($post as $item)
+        <tr>
+            <td>{{$item -> title}}</td>
+            <td>{{$item -> body}}</td>
+        </tr>
+        @php
+            $num++
+        @endphp
+        @endforeach
+    </table>
+    {{ $post->links() }}
+</body>
+</html>
