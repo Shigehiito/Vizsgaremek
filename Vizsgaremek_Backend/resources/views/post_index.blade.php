f4596e21d5fe201535813a57a78854fb6f15cc2f
Rendszergazda <rendszergazda@blathy.info>
2025-03-20 09:58:03 +0100

    changes to "post_index"

--- Vizsgaremek_Backend/resources/views/post_index.blade.php
+++ Vizsgaremek_Backend/resources/views/post_index.blade.php
@@ -9,23 +9,22 @@
     <form action="/post" method='GET'>
         <input type="text" name="search" class="form-control" placholder="Search">
     </form>
-    <div class="container">
-    @php
-        $num=1
-    @endphp
-    <table>
-        @foreach ($post as $item)
-        <tr>
-            <td>{{$item -> title}}</td>
-            <td>{{$item -> body}}</td>
-        </tr>
+    <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
         @php
-            $num++
+            $num=1
         @endphp
-        @endforeach
-    </table>
-    {{$post->links()}}
+        <table>
+            @foreach ($post as $item)
+            <tr>
+                <td>{{$item -> title}}</td>
+                <td>{{$item -> body}}</td>
+            </tr>
+            @php
+                $num++
+            @endphp
+            @endforeach
+        </table>
+        {{$post->links()}}
     </div>
-
 </body>
 </html>
