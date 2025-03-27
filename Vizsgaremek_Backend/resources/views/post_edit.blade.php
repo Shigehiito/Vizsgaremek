3441828ef490f98b7203d659f02ce93cb85076d5
Rendszergazda <rendszergazda@blathy.info>
2025-03-27 14:20:29 +0100

    made changes

--- Vizsgaremek_Backend/resources/views/post_edit.blade.php
+++ Vizsgaremek_Backend/resources/views/post_edit.blade.php
@@ -7,18 +7,18 @@
 </head>
 <body>
 <h1>Edit Post</h1>
-<form action='/post' method='POST'>
+<form action='' method='POST'>
     @csrf
     @method('PUT')
   <div class="mb-6">
     <label for="postTitle" class="form-label">Title</label>
-    <input type="text" class="form-control" id="postTitle" value="{{ $post->title }}">
+    <input type="text" class="form-control" id="postTitle"></input>
   </div>
   <div class="mb-6">
     <label for="postBody" class="form-label">Body</label>
   </div>
   <button type="submit" class="btn btn-primary">Create</button>
-  <textarea class="form-control" id="postBody">{{ $post->body }}</textarea>
+  <textarea class="form-control" id="postBody"></textarea>
 </form>
 </body>
 </html>
