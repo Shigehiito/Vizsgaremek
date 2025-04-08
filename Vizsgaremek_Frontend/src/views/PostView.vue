bfcbae9f6f34392b72b85603b88d1650056b0584
Rendszergazda <rendszergazda@blathy.info>
2025-04-08 09:33:03 +0200

    Created VUE.js frontend folder, so the design of the page can be done in the future.

---
+++ Vizsgaremek_Frontend/src/views/PostView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1>You can see the posts here. If you'd like to edit one of them, feel free to press the *Edit* button!</h1>
+</template>
+
+<style scoped>
+
+</style>
