d5cfd6285a4f7e3dd34f888bf196f3c7bc581367
Rendszergazda <rendszergazda@blathy.info>
2025-04-15 09:37:44 +0200

    made progress with "HomeView"

--- Vizsgaremek_Frontend/src/views/HomeView.vue
+++ Vizsgaremek_Frontend/src/views/HomeView.vue
@@ -1,6 +1,24 @@
 <script>
+import { http } from '@/utils/http';
 export default{
-    name: 'HomeView'
+    name: 'HomeView',
+    components:
+    "",
+    data(){
+        return {
+            posts:[]
+        }
+    },
+    methods:{
+        async getPost(){
+            const resp = await http.get(`/posts`);
+            this.posts = await resp.data.data;
+        }
+    },
+    mounted(){
+        this.getPost();
+    }
+
 }
 </script>
 
